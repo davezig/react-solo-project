9d60defc178fd11e21c20f2758864bae6884f89f
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-03-01 10:35:05 -0500

    started CSS on explore nearby

--- authenticate-me/frontend/src/components/ExploreNearby/index.js
+++ authenticate-me/frontend/src/components/ExploreNearby/index.js
@@ -6,11 +6,12 @@ import './ExploreNearby.css';
 
 const ExploreNearby = () => {
     return (
-        <div className="exploreNearby-main-div">
-            <div className="exploreNearby-title-text"><span>Explore Nearby</span></div>
-            <div className="exploreNearby-small-images"></div>
-            <img src={"https://a0.muscache.com/im/pictures/676c0a60-2a5a-4598-aeeb-10a81aa5232f.jpg?im_q=medq&im_w=240"}></img>
-            <div className="exploreNearby-descriptive-text"></div>
+        <div className="exploreNearby">
+            <p className="exploreNearby__header">Explore Nearby</p>
+            <div className="exploreNearby__small-images">
+                <img src={"https://a0.muscache.com/im/pictures/676c0a60-2a5a-4598-aeeb-10a81aa5232f.jpg?im_q=medq&im_w=240"}></img>
+            </div>
+            <div className="exploreNearby__small-images--descriptive-text"></div>
         </div>
     )
 }
