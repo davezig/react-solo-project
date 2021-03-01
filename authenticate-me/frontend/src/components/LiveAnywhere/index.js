e60a7909accf7d675d26362c62f1c8fb639cd310
bamgithub <48144766+bamgithub@users.noreply.github.com>
2021-03-01 10:17:19 -0500

    rounded images for Live Anywhere and minor CSS

--- authenticate-me/frontend/src/components/LiveAnywhere/index.js
+++ authenticate-me/frontend/src/components/LiveAnywhere/index.js
@@ -5,11 +5,11 @@ const LiveAnywhere = ({titleAndPicArray}) => {
 
     return (
         <div className="liveAnywhere">
-        <p className="liveAnywhere__header">Live anywhere test</p>
+        <p className="liveAnywhere__header">Live Anywhere</p>
         <div className="liveAnywhere__images">
             {titleAndPicArray.map(image => {
                 return (
-                    <div>
+                    <div className="liveAnywhere__images--img">
                         <img src={image.url}></img>
                         <p>{image.title}</p>
                     </div>
--- authenticate-me/frontend/src/components/SplashPage/index.js
+++ authenticate-me/frontend/src/components/SplashPage/index.js
@@ -6,9 +6,10 @@ import './SplashPage.css';
 import ExploreNearby from '../ExploreNearby';
 import LiveAnywhere from '../LiveAnywhere';
 
-const titleAndPicArray = [{title: 'Test Title1', url: 'https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480'},
-                            {title: 'Test Title2', url: 'https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=480'},
-                            {title: 'Test Title3', url: 'https://a0.muscache.com/im/pictures/a596c528-a2e9-4417-9bbe-ceb68b2f6b9c.jpg?im_w=480'}]
+const titleAndPicArray = [{title: 'Entire homes', url: 'https://a0.muscache.com/im/pictures/7d82ca14-56e5-4465-8218-dcfa7d69b6ac.jpg?im_w=480'},
+                            {title: 'Unique stays', url: 'https://a0.muscache.com/im/pictures/36f53e61-db8d-403c-9122-5b761c0e4264.jpg?im_w=480'},
+                            {title: 'Cabins and cottages', url: 'https://a0.muscache.com/im/pictures/a596c528-a2e9-4417-9bbe-ceb68b2f6b9c.jpg?im_w=480'},
+                            {title: 'Pets allowed', url: 'https://a0.muscache.com/im/pictures/10a638e1-6aff-4313-8033-1275cec83987.jpg?im_w=480'}]
 
 const SplashPage = () => {
     return (
